b36662323520c4f9e5264fb377e62b678a62e364
Seva Mouler <vpmouler@gmail.com>
2017-09-08 12:59:54 -0700

    completed state and refactored to have 2 components

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var App = () => (
 
 // why have to use .map and not forEach, youre not saving to an array?
 // this is the error we get (but it still renders): react-with-addons.js:20747 Warning: Each child in an array or iterator should have a unique "key" prop. Check the render method of `GroceryList`. See https://fb.me/react-warning-keys for more information.
-var GroceryList = (props) => (
+/*var GroceryList = (props) => (
     // make our unordered list
     <ul>
       {
@@ -16,8 +16,27 @@ var GroceryList = (props) => (
           )
       }
     </ul>
-  )
+  )*/
 
+class GroceryList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    // this.props.iterms is array
+    return (
+      <ul>
+      {
+        this.props.items.map(item => 
+          <GroceryListItems key={item} item={item} />
+        )
+      }
+      </ul>
+      )
+   }
+
+}
 
 // make a class that will render our grocery list items
 
@@ -32,9 +51,8 @@ class GroceryListItems extends React.Component {
   onListItemClick() {
     this.setState({
       done: !this.state.done
-    })
-    // this.state.done = !this.state.done;
-  } 
+    });
+  }
 
 
   render() {
@@ -44,11 +62,11 @@ class GroceryListItems extends React.Component {
     // };
 
     var style = {
-      textDecoration: this.state.done ? 'line-through' : 'none'
+      fontWeight: this.state.done ? 'bold' : 'normal'
     }
     // have to return a JSX bc have {} here
     return (
-      <li onClick={this.onListItemClick}> {this.props.item} </li>
+      <li style={style} onMouseEnter={this.onListItemClick.bind(this)} onMouseLeave={this.onListItemClick.bind(this)}> {this.props.item} </li>
       )
   }
 
