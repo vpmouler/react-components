19e9be873c31e3eac33a1e5e88507eb34254bc39
Seva Mouler <vpmouler@gmail.com>
2017-09-08 12:19:38 -0700

    completed .map loop instead of static index look up, also added key as per FB React specs on GitHUb

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 var App = () => (
     <div>
-      <GroceryList items={['Apple', 'Cinammon']} />
+      <GroceryList items={['Apple', 'Cinammon','buut']} />
     </div>
   )
 
+
+// why have to use .map and not forEach, youre not saving to an array?
+// this is the error we get (but it still renders): react-with-addons.js:20747 Warning: Each child in an array or iterator should have a unique "key" prop. Check the render method of `GroceryList`. See https://fb.me/react-warning-keys for more information.
 var GroceryList = (props) => (
     // make our unordered list
     <ul>
-      <GroceryListItems item={props.items[0]} />
-      <GroceryListItems item={props.items[1]} />
+      {
+        props.items.map(item => 
+          <GroceryListItems key={item} item={item} />
+          )
+      }
     </ul>
   )
 
